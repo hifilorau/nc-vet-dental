dba4e7d5b06697df5ba6757ffc686deb937318cf
hifilorau <tomrau@gmail.com>
2015-10-15 16:12:32 -0400

    checkin

---
+++ src/js/intro.js
@@ -0,0 +1,23 @@
+$(function() {
+  	$('.footer').hide();
+		$('.desktop-nav-container').hide();
+  	$('.hero-content-main').hide();
+	
+	$('.pet-button').click(function(){
+		$('.referral-overlay').hide("slide", { direction: "right" }, 2000);
+		$('.hero-content-wrapper').hide();
+		
+		setTimeout(function(){
+		$('.footer').show();
+		$('.desktop-nav-container').fadeIn(2000);
+		$('.hero-overlay').hide();
+		$('.hero-content-intro').hide();
+		$('.hero-content-main').fadeIn(2000);
+  
+}, 2000);
+		
+		
+	});
+	
+	
+});
