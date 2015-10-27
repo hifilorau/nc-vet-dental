402959db4552611f1cdb43ea9d43a83ffd3c8639
hifilorau <tomrau@gmail.com>
2015-10-27 13:08:17 -0400

    checkin added referrals and div fix

--- src/js/intro.js
+++ src/js/intro.js
@@ -2,22 +2,26 @@ $(function() {
   	// $('.footer').hide();
 		// $('.desktop-nav-container').hide();
   	$('.hero-content-main').hide();
+	  $('.referral').hide();
 	
+	
+	///// pet side intro
 	$('.pet-button').click(function(){
 		$('.referral-overlay').hide("slide", { direction: "right" }, 2000);
-		$('.hero-content-wrapper').hide();
-		
+		$('.hero-content-wrapper').hide();		
 		setTimeout(function(){
-		$('.footer').show();
-		$('.desktop-nav-container').fadeIn(2000);
-		$('.hero-overlay').hide();
-		$('.hero-content-intro').hide();
-		$('.hero-content-main').fadeIn(2000);
-  
-}, 2000);
-		
-		
+			$('.footer').show();
+			$('.desktop-nav-container').fadeIn(2000);
+			$('.hero-overlay').hide();
+			$('.hero-content-intro').hide();
+			$('.hero-content-main').fadeIn(2000);
+    }, 2000);	
 	});
 	
 	
+	///// vet side 
+	$('.vet-button').click(function(){
+		$('.referral').show();
+		
+	});
 });
