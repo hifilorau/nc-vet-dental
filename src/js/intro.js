66830835a6edce516c5cab6baf72cb60fefde2c2
hifilorau <tomrau@gmail.com>
2015-11-05 15:16:05 -0500

    checkin

--- src/js/intro.js
+++ src/js/intro.js
@@ -30,12 +30,14 @@ $(function() {
 		localStorage.setItem("siteState", siteState);
 		delayThings();
 		checkState();
-		$('.referral-overlay').hide("slide", { direction: "right" }, 2000);
-		$('.client-overlay').hide("slide", { direction: "left" }, 2000);
+		$('.referral-overlay').fadeOut({duration: 1000});
+		setTimeout(function(){}, 1000)
+		$('.client-overlay').fadeOut({ duration: 1000});
 //		setTimeout(function(){
 		$('.hero-content-main').html(
 			'<div class="referral-container"><h1 class="vet-referral-h1">Veterinary Referral Portal</h1><div class="vet-content"><div class="refer-row"><div class="table-head">Make Online Referral:</div><div class="table-content"><a href="referral.html">Referral Form</a></div></div><div class="refer-row"><div class="table-head">View Patient Records:</div><div class="table-content"><a target="_blank" href="https://www.dropbox.com">Record Portal</a></div></div><div class="refer-row"><div class="table-head">Pre-Anesthesia Screening:</div><p class="table-content">For all patients a recent veterinary examination, full chemistry panel, complete blood count and urinalysis are needed. For cats older then 8 years of age, a thyroid level is also needed. In addition, patients at risk for hypertension should have blood pressure measurements obtained. If patients have had a history of unexplained weight loss, then chest radiographs and abdominal ultrasound may be recommended prior to anesthesia.</p></div></div></div>'
 			);
+		$('.slides').css('display','none');
 //		},100);
 	});
 	
--- src/js/slide.js
+++ src/js/slide.js
@@ -2,6 +2,8 @@ $(function() {
 $('.slides').slick({
 	autoplay: true,
 	autoplaySpeed: 6000,
+	speed: 1000,
 	fade: true
+
 });
 });
